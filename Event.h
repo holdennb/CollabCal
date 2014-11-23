d8585a5581790a1dc726afd82b5e2f0b6e2c6868
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-11-23 15:16:59 -0800

    Finished Event Header

--- Event.h
+++ Event.h
@@ -7,7 +7,37 @@ class Event{
  private:
   /* The ID of the next created event. */
   static long int ID_COUNTER = 0;
+  /* The unique identifier for this event. */
+  long int id;
   /* The public name of the event. */
   std::string name;
+  /* The time at which the event occurs. */
+  time_t time;
+ public:
+  /* Create an event with the given name, occurring at the given time. */
+  Event(std::string name, time_t time);
+  /* Get the name of the event. */
+  std::string getName();
+  /* Get the time at which the event is occurring. */
+  time_t getTime();
+  /* Get the uniqe identifier for this event. */
+  long int getID();
+  /* Change the time at which this event is occurring. */
+  void reschedule(time_t newTime);
+  /* Change the name of the event. */
+  void rename(std::string newName);
+  /*
+     Serialize this event and write it to a file with the given filename,
+     overwriting any data that was previously there.
+     Returns true if the write succeeded, false otherwise.
+  */
+  bool writeToFile(std::string filename);
+  /*
+     Read an event from the file with the given filename.
+     Returns a pointer to the generated event.
+     Returns nullptr if the file could not be found,
+     or was not formatted properly.
+  */
+  static Event* readFromFile(std::string filename);
 }
 #endif
