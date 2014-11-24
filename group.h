9742da8fc128b93e4fc98027f16b4eb9453803de
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-11-23 17:55:07 -0800

    Fixed Up Header A Bit

--- group.h
+++ group.h
@@ -9,35 +9,38 @@
 class Group{
  private:
   /* The next group ID to use. */
-  static long int ID_COUNTER = 0;
+  static long ID_COUNTER;
   /* The id of this group. */
-  const long int id;
+  const long id;
   /* The users that belong to this group. */
-  std::list<long int> users;
+  std::list<long> users;
  public:
-  /* Create an empty group. */
-  Group();
   /* Create a group with some initial users in it. */
-  Group(const std::list<long int> &initialUsers);
+  Group(const std::list<long> &initialUsers = std::list<long>());
+
+  /* Recreate an existing group with a specific ID. */
+  Group(const std::list<long> &initialUsers = std::list<long>(),
+	const long id);
+  
   /* Get the unique identifier of this group. */
-  long int getID();
+  long getID();
   /*
      Get a list of user IDs cooresponding to the users
      that belong to this group.
   */
-  std::list<long int> getUsers();
+  std::list<long> getUserIDs();
   /*
      Add the existing user with the given ID to the group.
      Returns false if the user is already in the group,
      true otherwise.
   */
-  bool addUser(const long int userID);
+  bool addUser(const long userID);
   /* 
      Remove the user with the given ID from the group.
      returns false if the user is not already in the group,
      true otherwise.
   */
-  bool removeUser(const long int userID);
+  bool removeUser(const long userID);
   /*
      Write the group to a file with the given filename,
      overwriting any data that was previously in the file.
@@ -49,5 +52,5 @@ class Group{
      Returns nullptr if the file does not exist, or could not be read.
   */
   static Group* readFromFile(const std::string &filename);
-}
+};
 #endif
