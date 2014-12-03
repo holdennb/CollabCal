ae92c67c253d934c510039a5a11f7dfb38f0c536
holdennb <nickbholden@gmail.com>
2014-12-02 18:03:29 -0800

    updating events ajax

--- scripts.js
+++ scripts.js
@@ -106,19 +106,17 @@
 		    e.unbind();
 		});
 
-		
-		
+		// Query getEvents for the current user
+		(function poll(){
+		   setTimeout(function(){
+		      $.ajax({ url: "getEventsServer", success: function(data){
+		        updateEvents(JSON.parse(data);
 
-
-
-
-
-
-
-
-
-
-
+		        // Call this function (in 10 seconds)
+		        poll();
+		      }, dataType: "json"});
+		  }, 10000);
+		})();
 
 
 
@@ -128,6 +126,55 @@
 
 	});
 
+	function updateEvents(data) {
+		// draws events that fall within current month on calendar
+		//	(only if they are newer than the current event's timestamp?)
+		// data is JSON object:
+			// [
+			// 	{
+			// 		"id": "1"
+			// 		"name": "event1",
+			// 		"year": "2014",
+			// 		"month": "3",
+			// 		"day": "25",
+			// 		"time": "3:45"
+			// 	},
+			// 	{
+			// 		"id": "4"
+			// 		"name": "event4",
+			// 		"year": "2014",
+			// 		"month": "6",
+			// 		"day": "2",
+			// 		"time": "9:30"
+			// 	},
+			// ]
+			//		Should we add a timestamp field to event? then we can check that too
+
+		var curYear = parseInt($("#year").text());
+		var curMonth = parseInt($("#month").attr("data-value"));
+
+		// clear out inactive events
+		$("#table-div td.real-day .events:not(.active)").remove();
+
+		for (var event in data) {
+			if (event.year == curYear && event.month == curMonth) {
+				// it's in the right month, so append the event to the appropriate day
+
+				var td = $("#table-div td.real-day.day-" + event.day);
+
+				var newEvent = $("<div class='event'></div>");
+				newEvent.attr("data-value", event.id);
+				newEvent.append("<span class='time'>" + event.time + "</span>");
+				newEvent.append(": ");
+				newEvent.append("<span class='name'>" + event.name + "</span>");
+
+				td.append(newEvent);
+			}
+		}
+
+
+
+	}
 
 
 })();
