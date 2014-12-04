28f25cc76a768597aff22c41f8dbf57abeecd9ef
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-12-04 14:27:52 -0800

    Removed Unneccessary Includes

--- calenderServer.cpp
+++ calenderServer.cpp
@@ -1,15 +1,6 @@
 #include "calenderServer.h"
-#include "serverFiles.h"
 #include "serverActions.h"
 #include "persistentState.h"
-#include <iostream>
-#include <fstream>
-#include <map>
-#include <cstdio>
-#include <algorithm>
-#include <sstream>
-#include <sys/types.h>
-#include <sys/stat.h>
 
 using namespace std;
 
